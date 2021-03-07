0de585c8b885ed8cef6273b7b307ea6e12bbc197
Hymeck <noonimf@gmail.com>
2021-03-07 19:58:10 +0300

    purify (delete some stuff) entrypoint and client

--- src/Client.Console/PeerceClient.cs
+++ src/Client.Console/PeerceClient.cs
@@ -6,7 +6,7 @@ using GroupChat.Shared.Wrappers;
 
 namespace GroupChat.Client.Console
 {
-    public class PeerceClient
+    public partial class PeerceClient
     {
         public readonly string Username;
 
@@ -47,78 +47,6 @@ namespace GroupChat.Client.Console
                 return false;
             }
         }
-        
-        private void AddJoinRequestToQueue(GroupJoinRequest groupJoinRequest, IPEndPoint from)
-        {
-            if (_multicast == null)
-                return;
-            
-            // ignore request if we are not group creator and specified group id don't equal to this._groupId 
-            if (!_isGroupCreator || groupJoinRequest.GroupId != _groupId)
-                return;
-            
-            _joinRequestQueue.Enqueue(new JoinQueueElement(groupJoinRequest, from));
-        }
-
-        private void SetGroup(GroupJoinResponse groupJoinResponse)
-        {
-            // ignore if not success code
-            if (!groupJoinResponse.Code.IsSuccess())
-                return;
-
-            // ignore if already in this group
-            if (_groupId == groupJoinResponse.GroupId)
-                return;
-            
-            _groupId = groupJoinResponse.GroupId;
-            var groupEp = groupJoinResponse.GroupEndpoint.Value;
-                
-            ConfigureMulticast(groupEp.Address, groupEp.Port, GetLocalIpAddress());
-        }
-        
-        // private void OnBroadcastDatagramReceived(object sender, DatagramReceivedEventArgs e)
-        // {
-        //     var datagram = e.Datagram;
-        //     if (TryDeserializeDatagram(datagram, out GroupJoinRequest joinRequest))
-        //     {
-        //         AddJoinRequestToQueue(joinRequest, e.From);
-        //     }
-        //     
-        //     else if (TryDeserializeDatagram(datagram, out GroupJoinResponse joinResponse))
-        //     {
-        //         SetGroup(joinResponse);
-        //     }
-        // }
-
-        public void HandleJoinRequestQueue()
-        {
-            if (_multicast == null)
-                return;
-            
-            while (_joinRequestQueue.TryDequeue(out var el))
-            {
-                var request = new GroupJoinRequestEventArgs(
-                    el.Request.Username, 
-                    el.Request.GroupId, 
-                    el.Request.SentAt,
-                    el.From);
-                
-                GroupJoinRequestReceived?.Invoke(this, request);
-            }
-        }
-        
-        public void Accept(GroupJoinRequestEventArgs e)
-        {
-            if (_multicast == null)
-                return;
-
-            if (!_isGroupCreator || e.GroupId != _groupId)
-                return;
-            
-            var multicastEp = _multicast.DestinationEndpoint;
-            var joinResponse = new GroupJoinResponse(ResponseCode.Success, _groupId, multicastEp);
-            // _broadcast.Send(joinResponse.XmlSerialize(), e.From);
-        }
 
         private void ConfigureMulticast(IPAddress multicastIpAddress, int port, IPAddress localIpAddress)
         {
@@ -131,6 +59,37 @@ namespace GroupChat.Client.Console
 
             _joinRequestQueue = new ConcurrentQueue<JoinQueueElement>();
         }
+
+        public void CreateGroup(string groupId, IPAddress multicastIpAddress, int port = 9100)
+        {
+            _groupId = groupId;
+            _isGroupCreator = true;
+            
+            ConfigureMulticast(multicastIpAddress, port, GetLocalIpAddress());
+        }
+
+        private IPAddress GetLocalIpAddress() => _broadcast.LocalEndpoint.Address;
+        
+        
+        public void Close()
+        {
+            _broadcast?.UdpClient.Dispose();
+            _multicast?.Dispose();
+        }
+    }
+
+    #region group messaging
+
+    public partial class PeerceClient
+    {
+        public void SendMessage(string text)
+        {
+            if (_multicast == null)
+                return;
+
+            var msg = new GroupMessage(Username, text, DateTime.Now);
+            _multicast.Send(msg.XmlSerialize());
+        }
         
         private void OnMulticastDatagramReceived(object sender, DatagramReceivedEventArgs e)
         {
@@ -141,61 +100,19 @@ namespace GroupChat.Client.Console
             
             GroupMessageReceived?.Invoke(this, msgArgs);
         }
-
-        public void CreateGroup(string groupId, IPAddress multicastIpAddress, int port = 9100)
-        {
-            _groupId = groupId;
-            _isGroupCreator = true;
-            
-            ConfigureMulticast(multicastIpAddress, port, GetLocalIpAddress());
-        }
-
-        public void JoinGroup(string groupId, int timeout = 10000)
-        {
-            var joinRequest = new GroupJoinRequest(Username, groupId, DateTime.Now).XmlSerialize();
-            // _broadcast.Send(joinRequest);
-            _broadcast.UdpClient.Send(joinRequest, joinRequest.Length, _broadcast.DestinationEndpoint);
-
-            var previousTimeout = _broadcast.UdpClient.Client.ReceiveTimeout;
-            try
-            {
-                IPEndPoint creatorEp = null;
-                // _broadcast.UdpClient.Client.ReceiveTimeout = timeout;
-                
-                var result = _broadcast.UdpClient.Receive(ref creatorEp);
-                var response = result.XmlDeserialize<GroupJoinResponse>();
-                
-                SetGroup(response);
-            }
-
-            finally
-            {
-                _broadcast.UdpClient.Client.ReceiveTimeout = previousTimeout;
-            }
-        }
-
-        private IPAddress GetLocalIpAddress()
-        {
-            var localIpEp = (IPEndPoint)_broadcast.UdpClient.Client.LocalEndPoint;
-            return localIpEp.Address;
-        }
         
-        public void SendMessage(string text)
-        {
-            if (_multicast == null)
-                return;
-
-            var msg = new GroupMessage(Username, text, DateTime.Now);
-            _multicast.Send(msg.XmlSerialize());
-        }
-        
-        public void Close()
-        {
-            _broadcast?.UdpClient.Dispose();
-            _multicast?.Dispose();
-        }
-
-        public event EventHandler<GroupJoinRequestEventArgs> GroupJoinRequestReceived;
         public event EventHandler<GroupMessageEventArgs> GroupMessageReceived;
     }
+
+    #endregion group messaging
+
+    #region broadcast handling
+
+    public partial class PeerceClient
+    {
+        // public event EventHandler<GroupJoinRequestEventArgs> GroupJoinRequestReceived;
+    }
+
+    #endregion broadcast handling
+
 }
--- src/Client.Console/Program.cs
+++ src/Client.Console/Program.cs
@@ -11,9 +11,9 @@ namespace GroupChat.Client.Console
     {
         private static PeerceClient _peerceClient;
 
-        private static readonly CancellationTokenSource Cts = new();
-
+#pragma warning disable 1998
         static async Task Main(string[] args)
+#pragma warning restore 1998
         {
             if (args.Length == 0)
             {
@@ -21,100 +21,22 @@ namespace GroupChat.Client.Console
                 return;
             }
 
-            // TreatControlCAsInput = true; // otherwise the system will handle CTRL+C for us
-            CancelKeyPress += (_, e) =>
-            {
-                WriteLine("Cancelling...");
-                Cts.Cancel();
-                e.Cancel = true;
-            };
-
             var username = args[0];
             _peerceClient = new PeerceClient(username);
-            _peerceClient.GroupJoinRequestReceived += OnGroupJoinRequestReceived;
-            _peerceClient.GroupMessageReceived += OnGroupMessageReceived;
 
             // join case
             if (args.Length == 3)
             {
                 var groupId = args[2];
-                join:
-                try
-                {
-                    WriteLine("Waiting for response.");
-
-                    _peerceClient.JoinGroup(groupId);
-                }
-
-                catch (SocketException)
-                {
-                    WriteLine("No response. Try again? ([Y/n])");
-                    var answer = ReadLine()?.ToUpper();
-
-                    if (answer == "Y" || answer == "YES")
-                        goto join;
-                    
-                }
-
-                try
-                {
-                    Task.Run(MessagingLoop, Cts.Token).Wait(Cts.Token);
-                }
-
-                catch (OperationCanceledException)
-                {
-                    WriteLine("Bye.");
-                    _peerceClient.Close();
-                }
             }
 
             // create case
             else if (args.Length == 4)
             {
                 var groupId = args[2];
-                var multicastIpAddress = IPAddress.Parse(args[3]);
-                _peerceClient.CreateGroup(groupId, multicastIpAddress);
-
-                try
-                {
-                    Task.Run(MessagingLoop, Cts.Token).Wait(Cts.Token);
-                }
-
-                catch (OperationCanceledException)
-                {
-                    WriteLine("Bye.");
-                    _peerceClient.Close();
-                }
+                var multicastIp = IPAddress.Parse(args[3]);
             }
         }
-
-        private static async Task MessagingLoop()
-        {
-            if (!_peerceClient.IsGroupParticipant)
-            {
-                WriteLine("You are not participant of any group.");
-                return;
-            }
-
-            while (true)
-            {
-                // var text = ReadLine();
-                var text = await ReadLineAsync();
-
-                if (Cts.Token.IsCancellationRequested)
-                {
-                    Cts.Token.ThrowIfCancellationRequested();
-                    break;
-                }
-
-                _peerceClient.SendMessage(text);
-
-                // handle request queue
-                _peerceClient.HandleJoinRequestQueue();
-            }
-        }
-
-        private static async Task<string> ReadLineAsync() => await Task.Run(ReadLine, Cts.Token);
 
         private static void OnGroupMessageReceived(object sender, GroupMessageEventArgs e)
         {
@@ -124,23 +46,6 @@ namespace GroupChat.Client.Console
             WriteLine($" {e.Text}\n");
             WriteLine($"\t{e.SentAt.ToShortTimeString()}");
             WriteLine("===\n");
-        }
-
-        private static void OnGroupJoinRequestReceived(object sender, GroupJoinRequestEventArgs e)
-        {
-            WriteLine("+++");
-            WriteLine($"{e.GroupId}");
-            WriteLine($"{e.Username}");
-            WriteLine($"{e.From.Address}");
-            WriteLine($"\t{e.RequestedAt}");
-            WriteLine("+++");
-            WriteLine("Accept or deny? ([Y/n])");
-            WriteLine(e.ToString());
-
-            var answer = ReadLineAsync().Result.ToUpper();
-            // todo: fuck, i don't come to these lines
-            if (answer == "Y" || answer == "YES")
-                _peerceClient.Accept(e);
         }
 
         private static void PrintHelp()
